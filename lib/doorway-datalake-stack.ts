46a5b871465086abf6737cb119a41ac053e3e144
Glenn Brown <gbrown@bayareametro.gov>
2024-04-29 16:44:35 -0700

    adding testing and breaking into submodules

--- lib/doorway-datalake-stack.ts
+++ lib/doorway-datalake-stack.ts
@@ -1,32 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as dms from 'aws-cdk-lib/aws-dms'
-import * as ec2 from 'aws-cdk-lib/aws-ec2'
-import * as rds from 'aws-cdk-lib/aws-rds'
+import { RDSDBInstance } from './resources/database';
+
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DoorwayDatalakeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const vpc = new ec2.Vpc(this, "temp-vpc", {
-      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
-    })
-    const sg = new ec2.SecurityGroup(this, 'InboundPostgres', { vpc })
-    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.POSTGRES, "Allow PostgresPort")
-
-    const instance = new rds.DatabaseInstance(this, "temp-rds", {
-      engine: rds.DatabaseInstanceEngine.POSTGRES,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
-      credentials: rds.Credentials.fromGeneratedSecret('syscdk'),
-      vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
-      },
-      securityGroups: [sg]
-    })
-
-
-
+    const db = new RDSDBInstance(this, id, props).create(`${id}-db`)
 
 
 
--- test/doorway-datalake.test.ts
+++ test/doorway-datalake.test.ts
@@ -1,17 +1,26 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as DoorwayDatalake from '../lib/doorway-datalake-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as DoorwayDatalake from '../lib/doorway-datalake-stack';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/doorway-datalake-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new DoorwayDatalake.DoorwayDatalakeStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('RDS Database Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new DoorwayDatalake.DoorwayDatalakeStack(app, 'DBTestStack-db');
+    // THEN
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::RDS::DBInstance', {
+        DBInstanceClass: 'db.t3.micro'
+    });
 });
+// test('Lambda Created', () => {
+//     const app = new cdk.App();
+//     // WHEN
+//     const stack = new DoorwayDatalake.DoorwayDatalakeStack(app, 'LambdaTestStack');
+//     // THEN
+//     const template = Template.fromStack(stack);
+//     template.hasResourceProperties('AWS::Lambda::Function', {})
+
+// })
