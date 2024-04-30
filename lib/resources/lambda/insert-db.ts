6505d428028ea39c298e0ac4da21d71e143f844c
Glenn Brown <gbrown@bayareametro.gov>
2024-04-30 16:21:24 -0700

    using handler from demo

--- lib/resources/lambda/insert-db.ts
+++ lib/resources/lambda/insert-db.ts
@@ -1,3 +1,6 @@
-export function handler() {
-    console.log("Hello World!!!!")
-}
+import { Handler } from 'aws-lambda';
+
+export const handler: Handler = async (event, context) => {
+    console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+    return context.logStreamName;
+};
