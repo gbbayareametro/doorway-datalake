9749727cc19a749f15668ae1f1dc7a8c2ca41a7b
Glenn Brown <gbrown@bayareametro.gov>
2024-05-02 15:27:13 -0700

    refactoring vpc and db out of project

--- lib/doorway-datalake-stack.ts
+++ lib/doorway-datalake-stack.ts
@@ -1,20 +1,23 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 
 import { Construct } from "constructs";
-import { RDSDBInstance } from "./resources/database";
+import { RDSDBInstance } from "../../doorway-test-db/lib/resources/database";
 import { LambdaInstance } from "./resources/lambda";
-import { VpcInstance } from "./resources/vpc";
+import { VpcInstance } from "../../doorway-test-db/lib/resources/vpc";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DoorwayDatalakeStack extends Stack {
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    const vpc = new VpcInstance(this, id).create(`${id}-vpc`);
-    const db = new RDSDBInstance(this, id, props).create(`${id}-db`, vpc);
+    const vpc = Vpc.fromLookup(this,"VPC",{
+       vpcId: "vpc-0533af7fdb40791a7",
+    })
     const lambda = new LambdaInstance(this, id, props).create(
       `${id}-lambda`,
-      db.vpc,
+      vpc
     );
   }
 }
--- test/resources/lambda.test.ts
+++ test/resources/lambda.test.ts
@@ -1,14 +1,14 @@
 import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2"
 import { assert } from "console";
 import { LambdaInstance } from "../../lib/resources/lambda";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import { VpcInstance } from "../../lib/resources/vpc";
+import { VpcInstance } from "../../../doorway-test-db/lib/resources/vpc";
 
 test("Lambda Created", () => {
   const stack = new cdk.Stack();
   const id = "test-id";
-  const vpc = new VpcInstance(stack, id).create("test-vpc");
-
+  const vpc =  new ec2.Vpc(stack,"this")
   const mylambda = new LambdaInstance(stack, id).create("my-lambda", vpc);
   assert(mylambda.runtime == lambda.Runtime.NODEJS_18_X);
 });
