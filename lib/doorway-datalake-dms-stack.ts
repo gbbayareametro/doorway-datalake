400a3b0975c2a6056bc54cb1801b00867ea1c988
Glenn Brown <gbrown@bayareametro.gov>
2024-05-03 15:17:55 -0700

    adding DMS Endpoint

--- lib/doorway-datalake-dms-stack.ts
+++ lib/doorway-datalake-dms-stack.ts
@@ -1,8 +1,24 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { SecurityGroup } from "aws-cdk-lib/aws-ec2";
+import { DatabaseInstance, DatabaseSecret } from "aws-cdk-lib/aws-rds";
 import { Construct } from "constructs";
+import { EndpointType, PostgreSQLEndpoint } from "dms-patterns";
 
 export class DataLakeDMSStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+        const secret = DatabaseSecret.fromSecretAttributes(this,"secret",{
+            secretCompleteArn:"arn:aws:secretsmanager:us-east-2:364076391763:secret:DoorwayTestDbStackSecretCC3-dUp1tGcD6zhb-D6p5UJ"
+        });
+
+        const endpoint = new PostgreSQLEndpoint(this, "SourceEndpoint", {
+            endpointType: EndpointType.SOURCE,
+            databaseName: 'test',
+            endpointIdentifier: 'pgEndpoint',
+            postgresEndpointSettings: {
+                secretsManagerSecretId: secret.secretArn
+            }
+        });
     }
+
 }
