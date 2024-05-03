141a188f683621975b339625df47d823992b1799
Glenn Brown <gbrown@bayareametro.gov>
2024-05-03 10:21:41 -0700

    upping timeout

--- lib/resources/lambda.ts
+++ lib/resources/lambda.ts
@@ -1,13 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { IVpc, Vpc } from "aws-cdk-lib/aws-ec2";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as signer from "aws-cdk-lib/aws-signer";
-/*
-   This stack is really just a placeholder to mimic database writing for the DMS process before we hook it up to a live Doorway DB.
-   Its a lambda that just inserts new rows into a couple of dummy tables.
-*/
-import * as path from "path";
-import { handler } from "../../lambda-handler/insert-db";
+
 export class LambdaInstance {
   scope: cdk.Stack;
   id: string;
@@ -22,7 +16,8 @@ export class LambdaInstance {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'insert-db.handler',
       code: lambda.Code.fromAsset('./lambda-handler'),
-      vpc: vpc
+      vpc: vpc,
+      timeout: cdk.Duration.seconds(30)
 
     })
     return my_lambda;
