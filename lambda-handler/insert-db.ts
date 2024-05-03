0a36c6a129a15bcb3056364deef4d6bd943da1e3
Glenn Brown <gbrown@bayareametro.gov>
2024-05-03 09:57:48 -0700

    adding applications

--- lambda-handler/insert-db.ts
+++ lambda-handler/insert-db.ts
@@ -10,20 +10,37 @@ export const handler: Handler = async (event, context) => {
 };
 async function listingInsert(prisma: PrismaClient) {
 
+  for (let  i=0;i < 10; i++) {
+    const listing1 = await prisma.listing.create({
+      data: {
+        address: "123 main st.",
+        city: "Chicago",
+        state: "IL",
+        zip: "60640",
+      },
+    });
+    console.log(listing1)
+  }
+  const listings = await prisma.listing.findMany()
+  const count = listings.length
+  for (let  i=0;i < 10; i++) {
+    const myListing = listings[Math.floor(Math.random()*count)]
+    const applicant = prisma.application.create({
+      data: {
+        demo1: 'abcd',
+        demo2: 'defg',
+        DOB: new Date('1967-2-4'),
+        listing: myListing
 
-  const listing1 = await prisma.listing.create({
-    data: {
-      address: "123 main st.",
-      city: "Chicago",
-      state: "IL",
-      zip: "60640",
-    },
-  });
-  console.log(listing1)
+
+      }
+    })
+
+  }
+
+
 
 }
 async function getListings(prisma: PrismaClient) {
-  const listings = await prisma.listing.findMany()
-  console.log(listings)
 
 }
