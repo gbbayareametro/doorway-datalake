d02d0d0c065a0f437efe7cff16b0be85276df4fb
Glenn Brown <gbrown@bayareametro.gov>
2024-05-03 14:18:08 -0700

    Adding DMS VPC Role Stack

--- bin/doorway-datalake.ts
+++ bin/doorway-datalake.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DataLakeLambdaStack } from '../lib/doorway-datalake-lambda-stack';
+import { DataLakeDMSStack } from '../lib/doorway-datalake-dms-stack';
+import { DmsVpcRoleStack } from 'dms-patterns';
 
 const env = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
@@ -12,17 +14,8 @@ const env = {
 const app = new cdk.App();
 new DataLakeLambdaStack(app, 'DataLakeLambdaStack', {
   env:env
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+new DmsVpcRoleStack(app, 'DmsVpcRoleStack');
+// new DataLakeDMSStack(app, 'DataLakeLambdaStack', {
+//   env:env
+// });
