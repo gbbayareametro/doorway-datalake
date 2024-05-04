13f6844656e50cb0d2a190770304b54dda3c9b9a
Glenn Brown <gbrown@bayareametro.gov>
2024-05-04 16:17:43 -0700

    replication instance

--- lib/doorway-datalake-dms-stack.ts
+++ lib/doorway-datalake-dms-stack.ts
@@ -1,23 +1,7 @@
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import * as dms from "aws-cdk-lib/aws-dms";
-import { DatabaseInstance, DatabaseSecret } from "aws-cdk-lib/aws-rds";
-import {
-  BlockPublicAccess,
-  Bucket,
-  BucketEncryption,
-} from "aws-cdk-lib/aws-s3";
+import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnReplicationInstance } from "aws-cdk-lib/aws-dms";
+import { DatabaseSecret } from "aws-cdk-lib/aws-rds";
 import { Construct } from "constructs";
-import {
-  CapacityUnits,
-  EndpointType,
-  PostgreSQLEndpoint,
-  Postgres2S3,
-  ReplicationTypes,
-  S3TargetEndpoint,
-  SelectionAction,
-  SelectionRule,
-  TableMappings,
-} from "dms-patterns";
 
 export class DataLakeDMSStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -26,85 +10,14 @@ export class DataLakeDMSStack extends Stack {
       secretCompleteArn:
         "arn:aws:secretsmanager:us-east-2:364076391763:secret:DoorwayTestDbStackSecretCC3-dUp1tGcD6zhb-D6p5UJ",
     });
-
-    // const sourceEndpoint = new PostgreSQLEndpoint(this, "SourceEndpoint", {
-    //   endpointType: EndpointType.SOURCE,
-    //   databaseName: "test",
-    //   endpointIdentifier: "pgEndpoint",
-    //   postgresEndpointSettings: {
-    //     secretsManagerSecretId: secret.secretArn,
-    //   },
-    // });
-    const replicationSubnetGroup = new dms.CfnReplicationSubnetGroup(
-      this,
-      "ReplicationSubnetGroup",
-      {
-        replicationSubnetGroupDescription: "ReplicationSubnetGroup",
-        replicationSubnetGroupIdentifier: "mysqlreplicationsubnetID",
-        subnetIds: ["subnet-0e0c6bb5a6a2de244", "subnet-0da86c74b26023379"],
-      }
-    );
-
-    const computeConfig: dms.CfnReplicationConfig.ComputeConfigProperty = {
-      minCapacityUnits: CapacityUnits._1,
-      maxCapacityUnits: CapacityUnits._1,
-      multiAz: false,
-      replicationSubnetGroupId:
-        replicationSubnetGroup.replicationSubnetGroupIdentifier,
-      vpcSecurityGroupIds: ["sg-01b5ca28c998c0450"],
-    };
-    const tableMappings = new TableMappings([
-      new SelectionRule({
-        objectLocator: {
-          schemaName: "public",
-          tableName: "Listings",
-        },
-        ruleAction: SelectionAction.INCLUDE,
-      }),
-      new SelectionRule({
-        objectLocator: {
-          schemaName: "public",
-          tableName: "Applications",
-        },
-        ruleAction: SelectionAction.INCLUDE,
-      }),
-    ]);
-
-    const replicationBucket = new Bucket(this, "replicationBucket", {
-      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-      encryption: BucketEncryption.S3_MANAGED,
-      enforceSSL: true,
-      versioned: false,
-      removalPolicy: RemovalPolicy.DESTROY,
-    });
-    // const targetEndpoint = new S3TargetEndpoint(this,"testTargetEndpoint", {
-    //     bucketArn: replicationBucket.bucketArn
-
-    // });
-    // const replicationConfig = new dms.CfnReplicationConfig(
-    //   this,
-    //   "TestReplication",
-    //   {
-    //     computeConfig: computeConfig,
-    //     replicationConfigIdentifier: 'TestReplication',
-    //     replicationType: ReplicationTypes.FULL_LOAD_AND_CDC,
-    //     sourceEndpointArn: sourceEndpoint.ref,
-    //     tableMappings: tableMappings.format(),
-    //     targetEndpointArn: targetEndpoint.ref     }
-    // );
-    const postgres2s3 = new Postgres2S3(this,'Postgres2S3-test',{
-        bucketArn: replicationBucket.bucketArn,
-        databaseName: 'test',
-        postgresEndpointSettings:  {
-            secretsManagerSecretId: secret.secretArn,
-          },
-        tableMappings: tableMappings,
-        replicationConfigIdentifier: "TestReplication",
-        computeConfig: computeConfig,
-
+    const replicationInstance = new CfnReplicationInstance(this,'ReplicationInstance',{
+        replicationInstanceClass: 'dms.t2.micro',
+        publiclyAccessible: false,
+        vpcSecurityGroupIds:['sg-023f8c31e23696e1c']
 
 
 
     })
+
   }
 }
